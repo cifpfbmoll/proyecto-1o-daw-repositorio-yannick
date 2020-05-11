7f2d74d88075efe6153621e1bcd17dc5a55bc33b
yannickbf-prog <56312492+yannickbf-prog@users.noreply.github.com>
2020-05-11 21:13:41 +0200

    Commiteo con la practica un poco mas avanzada, he hecho un método para registrar usuarios, falta que impriman los datos de la persona que se ha registrado, en el caso de personas sin dni es algo mas complicado, que aun no se como solucionare por que necesito imprimir el id que tienen en la tabla.

--- ProyectoFinalEstacionEsqui/src/proyectofinalestacionesqui/ProyectoFinalEstacionEsqui.java
+++ ProyectoFinalEstacionEsqui/src/proyectofinalestacionesqui/ProyectoFinalEstacionEsqui.java
@@ -5,6 +5,15 @@
  */
 package proyectofinalestacionesqui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Yann
@@ -15,7 +24,14 @@ public class ProyectoFinalEstacionEsqui {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        menuPrincipal();
+        try {
+            //menuPrincipal();
+            crearUsuario();
+        } catch (SQLException ex) {
+            System.out.println(ex.getSQLState());
+            System.out.println(ex.getMessage());
+            Logger.getLogger(ProyectoFinalEstacionEsqui.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public static void menuPrincipal() {
@@ -27,4 +43,49 @@ public class ProyectoFinalEstacionEsqui {
         System.out.println("5- Consultar rutas por dificultad");
         System.out.println("6- Salir");
     } 
+    
+    public static void crearUsuario() throws SQLException {
+        //Pedimos los datos y los guardamos en un objeto
+        Scanner lector = new Scanner(System.in);
+        System.out.println("---- Crear usuario ----");
+        System.out.println("Dime tu DNI, si no tienes escribe 0");
+        String dni = lector.nextLine();
+        System.out.println("Dime tu nombre");
+        String nombre = lector.nextLine();
+        System.out.println("Dime tus apellidos");
+        String apellidos = lector.nextLine();
+        System.out.println("Dime tu fecha de nacimiento con el siguiente formato aaaa-mm-dd");
+        String fechaNacimiento = lector.next();
+        Usuario usuario = new Usuario();
+        usuario.setDni(dni);
+        usuario.setNombre(nombre);
+        usuario.setApellidos(apellidos);
+        usuario.setFecha_nacimiento(fechaNacimiento);
+        
+        //Parte en la que introducimos la informacion en la BD
+        Connection con = establecerConexion();
+        PreparedStatement usuarios = null;
+        String insertaCliente = "INSERT INTO clientes (dni, nombre, apellidos, fecha_nacimiento) VALUES (?, ?, ?, ?)";
+        try{   
+            usuarios = con.prepareStatement(insertaCliente);
+            con.setAutoCommit(false);
+            usuarios.setString(1,usuario.getDni());
+            usuarios.setString(2,usuario.getNombre());
+            usuarios.setString(3,usuario.getApellidos());
+            usuarios.setString(4,usuario.getFecha_nacimiento());
+            boolean n = usuarios.execute();
+            con.commit();
+        } catch (SQLException ex) {
+            System.out.println("SQLSTATE " + ex.getSQLState() + "SQLMESSAGE" +             ex.getMessage());
+            System.out.println("Hago rollback");
+            con.rollback();  
+        } finally{
+            con.setAutoCommit(true);
+            usuarios.close();
+        }
+    }
+    
+    public static Connection establecerConexion() throws SQLException{  
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/estacion_esqui", "root", "");  
+    }
 }
---
+++ ProyectoFinalEstacionEsqui/src/proyectofinalestacionesqui/Usuario.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalestacionesqui;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Yann
+ */
+public class Usuario implements PistasEsqui{
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String fecha_nacimiento;
+
+    public Usuario() {
+    }
+
+    public Usuario(String dni, String nombre, String apellidos, String fecha_nacimiento) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        //Pasamos el nombre por el metodo ponerMayusculasNombreApellido() para
+        //que nos guarde el nombre en mayusculas si el usuario no lo ha hecho
+        nombre = ponerMayusculasNombreApellido(nombre);
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        //Pasamos el apellido por el metodo ponerMayusculasNombreApellido() para
+        //que nos guarde el nombre en mayusculas si el usuario no lo ha hecho
+        apellidos = ponerMayusculasNombreApellido(apellidos);
+        this.apellidos = apellidos;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+    //Metodo para poner la primera letra en Mayuscula de un nombre compuesto o no
+    //o de los apellidos
+    public String ponerMayusculasNombreApellido(String nomAp){
+        //Pasamos el String de nombre o apellidos a un array de chars
+        char[] cfr = nomAp.toCharArray();
+        
+        //La primera letra siempre es mayuscula, asi que la cojemos y la guardamos
+        //en un String
+        String nomApFinal = nomAp.substring(0, 1).toUpperCase();
+        
+        //Aqui comprobamos si hay espacios (en el array de chars) a partir del 
+        //segundo caracter, si hay un espacio el caracter siguiente es cambiado
+        //a mayusculas despues guardamos todos los caracteres en un string pero
+        //esta vez con las Mayusculas en los nombres y apellidos
+        for(int i = 1; i<cfr.length; i++) {
+           if(cfr[i] == ' '){
+                cfr[i+1] = Character.toUpperCase(cfr[i+1]);
+           }
+           nomApFinal += cfr[i];
+        }
+        return nomApFinal;
+    }
+}
